3d7db368192a9e838dabcef516b942669c5beb45
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-05-14 01:33:54 +0900

    passport

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -1,8 +1,7 @@
-import { prisma } from "../../../../generated/prisma-client";
-
 export default {
 	Mutation: {
-		createAccount: async (_, args) => {
+		createAccount: async (_, args, { prisma }) => {
+			console.log(prisma);
 			const { username, email, firstName = "", lastName = "", bio = "" } = args;
 			const user = await prisma.createUser({ username, email, firstName, lastName, bio });
 			return user;
