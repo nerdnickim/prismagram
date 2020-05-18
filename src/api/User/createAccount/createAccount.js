d973e75dbb055f0ea366d2a27d60c27f1771058f
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-05-18 21:00:48 +0900

    authorization

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -1,10 +1,22 @@
+import { prisma } from "../../../../generated/prisma-client";
+
 export default {
 	Mutation: {
-		createAccount: async (_, args, { prisma }) => {
-			console.log(prisma);
+		createAccount: async (_, args) => {
 			const { username, email, firstName = "", lastName = "", bio = "" } = args;
-			const user = await prisma.createUser({ username, email, firstName, lastName, bio });
-			return user;
+			const exists = await prisma.$exists.user({
+				OR: [
+					{
+						username,
+					},
+					{ email },
+				],
+			});
+			if (exists) {
+				throw Error("This username/email is already taken");
+			}
+			await prisma.createUser({ username, email, firstName, lastName, bio });
+			return true;
 		},
 	},
 };
