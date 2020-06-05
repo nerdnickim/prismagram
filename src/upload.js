cc6a37f91c75a14bf11e0d99a65774c142de6927
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-06-05 18:44:57 +0900

    upload

--- src/upload.js
+++ src/upload.js
@@ -1,12 +1,31 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 
-const upload = multer({ dest: "uploads/" });
+const s3 = new aws.S3({
+	accessKeyId: process.env.ACCESS_ID,
+	secretAccessKey: process.env.ACCESS_SECRET,
+	region: "ap-northeast-2",
+});
 
+const upload = multer({
+	storage: multerS3({
+		s3,
+		bucket: "cloninggram",
+		metadata: function(req, file, cb) {
+			cb(null, { fieldName: file.fieldname });
+		},
+		key: function(req, file, cb) {
+			cb(null, Date.now().toString());
+		},
+	}),
+});
 export const uploadMiddleware = upload.single("file");
 
 export const uploadController = (req, res) => {
 	const {
-		file: { path },
+		file: { location },
 	} = req;
-	res.json({ path });
+	console.log(location);
+	res.json({ location });
 };
